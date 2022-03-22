fc643102d596fac7fd699a91b40ea3bbcdae0e29
rodrigoCodDev <rodrigo.oliveira200310@gmail.com>
2022-03-21 22:40:11 -0300

    First commit

---
+++ app/src/main/java/com/rcoddev/compiti/TaskEditorActivity.java
@@ -0,0 +1,14 @@
+package com.rcoddev.compiti;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TaskEditorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_editor);
+    }
+}
