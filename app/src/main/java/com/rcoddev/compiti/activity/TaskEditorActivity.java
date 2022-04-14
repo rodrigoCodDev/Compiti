8623e37715ca84709d7e3e6e51847710541f95fc
Rodrigo <rodrigo.oliveira200310@gmail.com>
2022-04-14 09:58:20 -0300

    Add menu_task_editor.xml

--- app/src/main/java/com/rcoddev/compiti/activity/TaskEditorActivity.java
+++ app/src/main/java/com/rcoddev/compiti/activity/TaskEditorActivity.java
@@ -3,6 +3,9 @@ package com.rcoddev.compiti.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.rcoddev.compiti.R;
 
@@ -12,5 +15,24 @@ public class TaskEditorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_editor);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_task_editor, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.check_task:
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
